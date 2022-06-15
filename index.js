0334da8165ea4439d0911957d1f2ca7cc9426e29
thoma138 <thomaselliott@live.com>
2022-06-15 16:15:01 +1000

    commit tree 8d3962b3c9e225eea7c18e544df2586ce96022f8

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Code your solutions in this file
+
+
+function writeCards(names, eventName) {
+    for (let i = 0; i < names; i++) {
+        return [`Thankyou, ${names[i]}, for the wonderful ${eventName} gift!`]
+    }
+}
+
